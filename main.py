fe888a58d6c7ea5f5775a6d58f8f83b84f24fa15
FormulaCashew <acerty19@gmail.com>
2025-10-20 14:53:41 -0600

    Updated general flow and arguments

--- main.py
+++ main.py
@@ -10,29 +10,30 @@ from sklearn.preprocessing import LabelEncoder
 from Imputation import DataFrameImputation
 from KNN_Model import KNN
 
-path_to_df = "../../Datasets/titanic.csv"
-
-def fill_data(df: pd.DataFrame, exclusions : list = None) -> pd.DataFrame:
+def fill_data(df: pd.DataFrame, exclusions: list = None) -> pd.DataFrame:
 
     """
     Simple function to fill missing values in a dataframe by doing an imputation
     Args:
         df (pd.DataFrame): Dataframe to fill
-        exclusions (list): List of column names to exclude
     Returns:
         pd.DataFrame: Dataframe filled
     """
 
     # Process starting data
     print(df.head())
-    # Drop not necessary values
-    df = df.drop(columns=exclusions)
+    if exclusions:
+        df = df.drop(columns = exclusions, errors='ignore')
     # Data needs to be imputed
     imputator = DataFrameImputation(df)
     print(imputator.column_resume())
     imputator.plot_distribution(cols=['age', 'fare'], prefix="Before")
-    imputator.impute_data(method='knn')
+    imputator.impute_data(method='knn', k_neighbors=5)
     imputed_df = imputator.get_df()
+
+    # Encode sex
+    imputed_df['sex'] = imputed_df['sex'].map({'male': 0, 'female': 1})
+
     print(imputed_df.head())
     imputator.compare_distributions(imputator.original_df, imputed_df, columns=['age'], labels=('Before', 'After'), plot_type='hist')
     imputator.compare_distributions(imputator.original_df, imputed_df, columns=['age'], labels = ('Before', 'After'), plot_type='box')
@@ -42,16 +43,15 @@ def fill_data(df: pd.DataFrame, exclusions : list = None) -> pd.DataFrame:
         print(f"Successfully saved the cleaned data to '{output_filename}'")
     except Exception as e:
         print(f"Error saving file: {e}")
-    return pd.read_csv(output_filename)
+    return imputed_df
 
 def main():
+    path_to_df = "../../Datasets/titanic.csv"
     init_df = pd.read_csv(path_to_df)
 
     #imputation
-    titanic_df = fill_data(init_df, ['deck', 'embark_town', 'boat', 'body', 'home.dest']) # when there is incomplete data
-
-    # Encode sex
-    titanic_df['sex'] = titanic_df['sex'].map({'male': 0, 'female': 1})
+    exclusions = ['deck', 'embark_town', 'boat', 'body', 'home.dest']
+    titanic_df = fill_data(init_df, exclusions) # when there is incomplete data
 
     # The main features are: age, fare, pclass, sex, sibsp, parch
     features = ['age', 'fare', 'pclass', 'sex', 'sibsp', 'parch']
@@ -60,13 +60,18 @@ def main():
     inputs = titanic_df[features]
     outputs = titanic_df[to_find]
 
+    # Normalize only the inputs
+    process = DataFrameImputation(inputs)
+    process.normalize()
+    normalized_inputs = process.get_df()
+
     # Split data to test for precision
-    inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(inputs, outputs, test_size=0.2, random_state=42, stratify = outputs)
+    inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(normalized_inputs, outputs, test_size=0.2, random_state=42, stratify = outputs)
 
 
     knn_model : KNN = KNN(k=3)
-
     knn_model.store(inputs_train, outputs_train)
+
     predictions = knn_model.predict(inputs_test)
     correct_predictions = np.sum(predictions == np.array(outputs_test))
     accuracy = correct_predictions / len(outputs_test)
