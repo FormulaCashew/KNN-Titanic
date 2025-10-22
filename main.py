b6a316873bb483ab689033548b9182d64b61e833
FormulaCashew <acerty19@gmail.com>
2025-10-21 18:50:32 -0600

    Added function for plotting a confussion matrix

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 from platform import processor
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
@@ -14,6 +15,65 @@ from KNN_Model import KNN
 # Define
 impute_data = True
 path_to_df = "../../Datasets/titanic.csv"
+
+def plot_confusion_matrix(y_true, y_pred):
+    """
+    Generates, displays and saves a confusion matrix heatmap from the data given
+    Args:
+        y_true (array-like): A list of the true values
+        y_pred (array-like): A list of the predicted values
+    """
+    # Confusion matrix
+    correlation_mat = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(data=correlation_mat,
+                annot=True,
+                fmt="d",
+                cmap="GnBu",
+                xticklabels=['Died', 'Survived'],
+                yticklabels=['Died', 'Survived'])
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title("Correlation Matrix")
+    plt.tight_layout()
+    plt.show()
+    plt.savefig("./confusion_matrix.png", dpi=400)
+
+def fill_data(df: pd.DataFrame, exclusions: list = None) -> pd.DataFrame:
+    """
+    Simple function to fill missing values in a dataframe by doing an imputation
+    Uses DataFrameImputation class which is a local class made for this code, should be able to work with other dataframes
+    Args:
+        df (pd.DataFrame): Dataframe to fill
+        exclusions (list): List of column names to exclude
+    Returns:
+        pd.DataFrame: Dataframe filled
+    """
+
+    # Process starting data
+    print(df.head())
+    if exclusions:
+        df = df.drop(columns = exclusions, errors='ignore')
+    # Data needs to be imputed
+    imputator = DataFrameImputation(df)
+    print(imputator.column_resume())
+    imputator.plot_distribution(cols=['age', 'fare'], prefix="Before")
+    imputator.impute_data(method='knn', k_neighbors=5)
+    imputed_df = imputator.get_df()
+
+    # Encode sex
+    imputed_df['sex'] = imputed_df['sex'].map({'male': 0, 'female': 1})
+
+    print(imputed_df.head())
+    imputator.compare_distributions(imputator.original_df, imputed_df, columns=['age'], labels=('Before', 'After'), plot_type='hist')
+    imputator.compare_distributions(imputator.original_df, imputed_df, columns=['age'], labels = ('Before', 'After'), plot_type='box')
+    output_filename = 'titanic_imputed.csv'
+    try:
+        imputed_df.to_csv(output_filename, index=False)
+        print(f"Successfully saved the cleaned data to '{output_filename}'")
+    except Exception as e:
+        print(f"Error saving file: {e}")
+    return imputed_df
 
 def main():
     init_df = pd.read_csv(path_to_df)
@@ -58,44 +118,7 @@ def main():
     accuracy = correct_predictions / len(outputs_test)
     print(f"Accuracy on the test set: {accuracy:.3f}")
 
-    for item in predictions:
-        print(item)
+    plot_confusion_matrix(outputs_test, predictions)
 
 if __name__ == '__main__':
     main()
-
-def fill_data(df: pd.DataFrame, exclusions: list = None) -> pd.DataFrame:
-    """
-    Simple function to fill missing values in a dataframe by doing an imputation
-    Uses DataFrameImputation class which is a local class made for this code, should be able to work with other dataframes
-    Args:
-        df (pd.DataFrame): Dataframe to fill
-        exclusions (list): List of column names to exclude
-    Returns:
-        pd.DataFrame: Dataframe filled
-    """
-
-    # Process starting data
-    print(df.head())
-    if exclusions:
-        df = df.drop(columns = exclusions, errors='ignore')
-    # Data needs to be imputed
-    imputator = DataFrameImputation(df)
-    print(imputator.column_resume())
-    imputator.plot_distribution(cols=['age', 'fare'], prefix="Before")
-    imputator.impute_data(method='knn', k_neighbors=5)
-    imputed_df = imputator.get_df()
-
-    # Encode sex
-    imputed_df['sex'] = imputed_df['sex'].map({'male': 0, 'female': 1})
-
-    print(imputed_df.head())
-    imputator.compare_distributions(imputator.original_df, imputed_df, columns=['age'], labels=('Before', 'After'), plot_type='hist')
-    imputator.compare_distributions(imputator.original_df, imputed_df, columns=['age'], labels = ('Before', 'After'), plot_type='box')
-    output_filename = 'titanic_imputed.csv'
-    try:
-        imputed_df.to_csv(output_filename, index=False)
-        print(f"Successfully saved the cleaned data to '{output_filename}'")
-    except Exception as e:
-        print(f"Error saving file: {e}")
-    return imputed_df
